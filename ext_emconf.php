923bf4b9dcb0697efdbc627979458081487c1790
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-03-28 13:39:59 +0000

    Released to TER with obsolete status
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/remote_server/trunk@73890 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,18 +1,18 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext: "remote_server"
-#
-# Auto generated 16-03-2009 13:51
-#
-# Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+ * Extension Manager/Repository config file for ext "remote_server".
+ *
+ * Auto generated 28-03-2013 14:39
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Remote Server',
-	'description' => 'Enable TYPO3 to act as a remote application server for web services calls. BE access restrictions apply.',
+	'description' => 'Enable TYPO3 to act as a remote application server for web services calls. BE access restrictions apply. Not maintained anymore.',
 	'category' => 'be',
 	'author' => 'Francois Suter (Cobweb)',
 	'author_email' => 'typo3@cobweb.ch',
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'conflicts' => '',
 	'priority' => '',
 	'module' => '',
-	'state' => 'alpha',
+	'state' => 'obsolete',
 	'internal' => '',
 	'uploadfolder' => 0,
 	'createDirs' => '',
@@ -29,16 +29,17 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.2.0',
+	'version' => '0.2.1',
 	'constraints' => array(
 		'depends' => array(
+			'typo3' => '4.2.0-4.5.99',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:14:{s:9:"ChangeLog";s:4:"9469";s:10:"README.txt";s:4:"edf1";s:36:"class.tx_remoteserver_beuserauth.php";s:4:"a075";s:30:"class.tx_remoteserver_helo.php";s:4:"f472";s:34:"class.tx_remoteserver_response.php";s:4:"ce7b";s:8:"conf.php";s:4:"be6a";s:21:"ext_conf_template.txt";s:4:"ac4b";s:12:"ext_icon.gif";s:4:"e0f4";s:17:"ext_localconf.php";s:4:"5d28";s:9:"index.php";s:4:"125e";s:8:"init.php";s:4:"f77d";s:8:"json.php";s:4:"e081";s:14:"doc/manual.sxw";s:4:"d511";s:18:"samples/caller.php";s:4:"26fa";}',
+	'_md5_values_when_last_written' => 'a:14:{s:9:"ChangeLog";s:4:"d2c4";s:36:"class.tx_remoteserver_beuserauth.php";s:4:"a075";s:30:"class.tx_remoteserver_helo.php";s:4:"f472";s:34:"class.tx_remoteserver_response.php";s:4:"ce7b";s:8:"conf.php";s:4:"be6a";s:21:"ext_conf_template.txt";s:4:"ac4b";s:12:"ext_icon.gif";s:4:"e0f4";s:17:"ext_localconf.php";s:4:"5d28";s:9:"index.php";s:4:"125e";s:8:"init.php";s:4:"f77d";s:8:"json.php";s:4:"e081";s:10:"README.txt";s:4:"edf1";s:14:"doc/manual.sxw";s:4:"d511";s:18:"samples/caller.php";s:4:"26fa";}',
 	'suggests' => array(
 	),
 );
